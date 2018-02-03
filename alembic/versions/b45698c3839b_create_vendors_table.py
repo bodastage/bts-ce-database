305c2d5d33420c1f7c856c9c3ce75dedf94c0def
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-04 01:55:24 +0300

    Add default vendor list

--- alembic/versions/b45698c3839b_create_vendors_table.py
+++ alembic/versions/b45698c3839b_create_vendors_table.py
@@ -19,7 +19,7 @@ depends_on = None
 def upgrade():
     op.create_table(
         'vendors',
-        sa.Column('pk', sa.Integer, primary_key=True),
+        sa.Column('pk', sa.Integer, primary_key=True,  nullable=False),
         sa.Column('name', sa.String(50), nullable=False),
         sa.Column('notes', sa.Text),
         sa.Column('supported', sa.Boolean),
@@ -30,7 +30,26 @@ def upgrade():
     )
     op.execute('ALTER SEQUENCE  vendors_pk_seq RENAME TO seq_vendors_pk')
 
-    # op.bulk_insert()
+    vendors = sa.sql.table('vendors',
+                                sa.Column('pk', sa.Integer, sa.Sequence('seq_vendors_pk', ), primary_key=True,
+                                          nullable=False),
+                                sa.Column('name', sa.String(50), unique=True, nullable=False),
+                                sa.Column('notes', sa.Text),
+                                sa.Column('supported', sa.Boolean),
+                                sa.Column('modified_by', sa.Integer),
+                                sa.Column('added_by', sa.Integer),
+                                sa.Column('date_added', sa.TIMESTAMP, default=datetime.datetime.utcnow,
+                                          onupdate=datetime.datetime.utcnow),
+                                sa.Column('date_modified', sa.TIMESTAMP, default=datetime.datetime.utcnow)
+                            )
+
+    op.bulk_insert(vendors, [
+        {'name': 'Ericsson', 'supported': True, 'modified_by': 0, 'added_by': 0},
+        {'name': 'Huawei', 'supported': True, 'modified_by': 0, 'added_by': 0},
+        {'name': 'ZTE', 'supported': False, 'modified_by': 0, 'added_by': 0},
+        {'name': 'Nokia', 'supported': False, 'modified_by': 0, 'added_by': 0},
+        {'name': 'Bodastage', 'supported': True, 'modified_by': 0, 'added_by': 0},
+    ])
 
 
 def downgrade():
