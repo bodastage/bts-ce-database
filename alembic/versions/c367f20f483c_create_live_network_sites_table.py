559840f8a5a32043558a43568ca4937cf9888b32
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-05 03:11:03 +0300

    Create live network sites table

---
+++ alembic/versions/c367f20f483c_create_live_network_sites_table.py
@@ -0,0 +1,39 @@
+"""create live_network sites table
+
+Revision ID: c367f20f483c
+Revises: d66c88e81543
+Create Date: 2018-02-05 03:08:50.437000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import datetime
+
+# revision identifiers, used by Alembic.
+revision = 'c367f20f483c'
+down_revision = 'd66c88e81543'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'sites',
+        sa.Column('pk', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(100), nullable=False),
+        sa.Column('notes', sa.Text),
+        sa.Column('node_pk', sa.Integer),
+        sa.Column('tech_pk', sa.Integer),
+        sa.Column('vendor_pk', sa.Integer),
+        sa.Column('modified_by', sa.Integer),
+        sa.Column('added_by', sa.Integer),
+        sa.Column('date_added', sa.TIMESTAMP, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow),
+        sa.Column('date_modified', sa.TIMESTAMP, default=datetime.datetime.utcnow),
+        schema=u'live_network'
+    )
+    op.execute('ALTER SEQUENCE  sites_pk_seq RENAME TO seq_sites_pk')
+
+
+def downgrade():
+    op.drop_table('sites')
+    op.execute('DROP SEQUENCE seq_sites_pk')
