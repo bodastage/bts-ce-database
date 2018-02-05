f7a9ddeeea016caa3afdb5f5bae6221906b086a1
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-05 13:47:40 +0300

    Create live network gsm cells data table

--- alembic/versions/920b08e9f6ec_create_live_network_gsm_cells_data.py
+++ alembic/versions/920b08e9f6ec_create_live_network_gsm_cells_data.py
@@ -17,8 +17,34 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'gsm_cells_data',
+        sa.Column('pk', sa.Integer, primary_key=True),
+        sa.Column('name', sa.String(100), nullable=False),
+        sa.Column('cell_pk', sa.Integer, nullable=False),
+        sa.Column('bcc', sa.Integer,),
+        sa.Column('ncc', sa.Integer,),
+        sa.Column('bsic', sa.Integer,),
+        sa.Column('bcch', sa.Integer,),
+        sa.Column('lac', sa.Integer,),
+        sa.Column('latitude', sa.Float,),
+        sa.Column('longitude', sa.Float,),
+        sa.Column('cgi', sa.String(200),),
+        sa.Column('azimuth', sa.Integer,),
+        sa.Column('height', sa.Integer,),
+        sa.Column('mechanical_tilt', sa.Integer,),
+        sa.Column('electrical_tilt', sa.Integer,),
+        sa.Column('hsn', sa.Integer, ),
+        sa.Column('hopping_type', sa.String(100), ),
+        sa.Column('tch_carriers', sa.String(255), ),
+        sa.Column('modified_by', sa.Integer),
+        sa.Column('added_by', sa.Integer),
+        sa.Column('date_added', sa.TIMESTAMP, default=sa.func.now(), onupdate=sa.func.now()),
+        sa.Column('date_modified', sa.TIMESTAMP, default=sa.func.now()),
+        schema=u'live_network'
+    )
+    op.execute('ALTER SEQUENCE  live_network.gsm_cells_data_pk_seq RENAME TO seq_gsm_cells_data_pk')
 
 
 def downgrade():
-    pass
+    op.drop_table('gsm_cells_data', schema=u'live_network')
