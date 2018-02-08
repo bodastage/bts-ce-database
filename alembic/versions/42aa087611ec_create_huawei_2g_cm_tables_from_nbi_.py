5c0a663f5019b59e6e0f2d844cf92972a82f0005
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-02-08 04:48:02 +0300

    import postgres dialect types in
    42aa087611ec_create_huawei_2g_cm_tables_from_nbi_.py

--- alembic/versions/42aa087611ec_create_huawei_2g_cm_tables_from_nbi_.py
+++ alembic/versions/42aa087611ec_create_huawei_2g_cm_tables_from_nbi_.py
@@ -7,7 +7,7 @@ Create Date: 2018-02-08 03:38:26.018000
 """
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '42aa087611ec'
