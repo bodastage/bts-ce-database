2097b5bc962b25edf4bb412d1c8a4c485104a0a5
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-05-23 08:11:48 +0300

    Create table for LTE external definitions

--- alembic/versions/5b8d91271e45_create_table_for_4g_external_cells.py
+++ alembic/versions/5b8d91271e45_create_table_for_4g_external_cells.py
@@ -1,8 +1,8 @@
-"""create live network gsm external cells table
+"""Create table for 4G external cells
 
-Revision ID: 0e0ceb9a3832
-Revises: 24e5cd9f23c2
-Create Date: 2018-02-05 14:31:35.335000
+Revision ID: 5b8d91271e45
+Revises: 0588ebb5527a
+Create Date: 2018-05-23 07:29:17.642000
 
 """
 from alembic import op
@@ -10,35 +10,33 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0e0ceb9a3832'
-down_revision = '24e5cd9f23c2'
+revision = '5b8d91271e45'
+down_revision = '0588ebb5527a'
 branch_labels = None
 depends_on = None
 
 
+
 def upgrade():
     op.create_table(
-        'gsm_external_cells',
+        'lte_external_cells',
         sa.Column('pk', sa.Integer, primary_key=True),
         sa.Column('name', sa.String(100), nullable=False),
         sa.Column('cell_pk', sa.Integer, nullable=False),
         sa.Column('node_pk', sa.Integer,),
-        sa.Column('mcc', sa.Integer,),
+        sa.Column('dl_earfcn', sa.Integer,),
         sa.Column('mnc', sa.Integer,),
-        sa.Column('lac', sa.Integer,),
-        sa.Column('bcch', sa.String(200),),
-        sa.Column('ncc', sa.Integer,),
-        sa.Column('bcc', sa.Integer,),
-        sa.Column('ci', sa.Integer,),
-        sa.Column('rac', sa.Integer, ),
+        sa.Column('mcc', sa.Integer, ),
+        sa.Column('localCellId', sa.Integer,),
+        sa.Column('pci', sa.Integer, ),
         sa.Column('modified_by', sa.Integer),
         sa.Column('added_by', sa.Integer),
         sa.Column('date_added', sa.TIMESTAMP, default=sa.func.now(), onupdate=sa.func.now()),
         sa.Column('date_modified', sa.TIMESTAMP, default=sa.func.now()),
         schema=u'live_network'
     )
-    op.execute('ALTER SEQUENCE  live_network.gsm_external_cells_pk_seq RENAME TO seq_gsm_external_cells_pk')
+    op.execute('ALTER SEQUENCE  live_network.lte_external_cells_pk_seq RENAME TO seq_lte_external_cells_pk')
 
 
 def downgrade():
-    op.drop_table('gsm_external_cells', schema=u'live_network')
+    op.drop_table('lte_external_cells', schema=u'live_network')
