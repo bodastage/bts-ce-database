7ed277b45c4fae8cb6f5bd82cd25c7b99e4f0f0b
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-11-18 15:49:45 +0300

    Add nodename to vw_lte_external_cells

--- alembic/versions/a5ff5b06e303_create_live_network_lte_external_cells.py
+++ alembic/versions/a5ff5b06e303_create_live_network_lte_external_cells.py
@@ -25,8 +25,8 @@ class ReplaceableObject(object):
 vw_lte_external_cells = ReplaceableObject(
     'live_network.vw_lte_external_cells',
     """
-     SELECT 
-        t3.name as sitename,
+     SELECT t4.name AS nodename,
+       t3.name AS sitename,
         t1.name AS cellname,
         t1.mnc,
         t1.mcc,
@@ -34,8 +34,9 @@ vw_lte_external_cells = ReplaceableObject(
         t1.pci,
         t1.tac
        FROM live_network.lte_external_cells t1
-       LEFT JOIN live_network.cells t2 ON t1.cell_pk = t2.pk
-       LEFT JOIN live_network.sites t3 ON t3.pk = t2.site_pk
+         LEFT JOIN live_network.cells t2 ON t1.cell_pk = t2.pk
+         LEFT JOIN live_network.sites t3 ON t3.pk = t2.site_pk
+      LEFT JOIN live_network.nodes t4 ON t4.pk = t1.node_pk
     """)
 
 def upgrade():
