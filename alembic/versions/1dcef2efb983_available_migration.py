9964e488e205632728ef21049a55a84f1458e6ed
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2018-12-29 20:59:15 +0300

    Add ZTE cm load tables

---
+++ alembic/versions/1dcef2efb983_available_migration.py
@@ -0,0 +1,23 @@
+"""Available migrations
+
+Revision ID: 1dcef2efb983
+Revises: 74dfd1cca7a4
+Create Date: 2018-04-22 22:22:04.887000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1dcef2efb983'
+down_revision = '74dfd1cca7a4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
--- alembic/versions/aaf2420bd403_create_huawei_mml_insert_queries.py
+++ alembic/versions/aaf2420bd403_create_huawei_mml_insert_queries.py
@@ -1,8 +1,8 @@
 """Create Huawei MML insert queries
 
-Revision ID: 1dcef2efb983
-Revises: 74dfd1cca7a4
-Create Date: 2018-04-22 22:22:04.887000
+Revision ID: aaf2420bd403
+Revises: 83bac4c4058e
+Create Date: 2018-12-24 15:04:41.217968
 
 """
 from alembic import op
@@ -10,10 +10,11 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1dcef2efb983'
-down_revision = '74dfd1cca7a4'
+revision = 'aaf2420bd403'
+down_revision = '83bac4c4058e'
 branch_labels = None
 depends_on = None
+
 
 
 def upgrade():
