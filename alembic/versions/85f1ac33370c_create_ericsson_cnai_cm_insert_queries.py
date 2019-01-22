2c904a308cd0a35e007b1bebce005464eed5badb
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2019-01-23 02:33:56 +0300

    Create Ericsson CNAIV2 load tables and insert queries

---
+++ alembic/versions/85f1ac33370c_create_ericsson_cnai_cm_insert_queries.py
@@ -0,0 +1,24 @@
+"""Create Ericsson CNAI CM insert queries
+
+Revision ID: 85f1ac33370c
+Revises: 1085dca2618a
+Create Date: 2019-01-23 02:29:10.993859
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '85f1ac33370c'
+down_revision = '1085dca2618a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
